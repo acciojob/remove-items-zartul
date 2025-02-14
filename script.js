f8ed21f58aca2236e1fce5499c781a86b8e969d0
zartul <121471819+zartul@users.noreply.github.com>
2025-02-14 09:48:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+// const selectElem = document.getElementById("colorSelect");
+// const pElem = document.getElementById("p");
+
+// selectElem.addEventListener("change", () => {
+//   const index = selectElem.selectedIndex;
+//   pElem.textContent = `selectedIndex: ${index}`;
+// });
+function removeColor() {
+    const selectElem = document.getElementById("colorSelect");
+    selectElem.remove(selectElem.selectedIndex); // Remove the selected option
+}
